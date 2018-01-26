42e6a617a0ebf13d52407ba46b183018c97ad364
Michel <michelpl@yahoo.com.br>
2018-01-26 19:49:35 -0200

    Add multi store support.

--- injected.php
+++ injected.php
@@ -1,2 +1,16 @@
 <?php
-$data['frontlinkUrl'] = $data['frontlinkUrl'] = HTTPS_CATALOG;
+$data['stores'][] = [
+    'name' => $this->config->get('config_name'),
+    'href' => HTTP_CATALOG
+];
+
+$this->load->model('setting/store');
+
+$results = $this->model_setting_store->getStores();
+
+foreach ($results as $result) {
+    $data['stores'][] = [
+        'name' => $result['name'],
+        'href' => $result['url']
+    ];
+}
